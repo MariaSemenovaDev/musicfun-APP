0f5e2507f518591f899d9f81f673ea9cd5a3983d
Maria Semenova <semenovamk96@mail.ru>
2025-09-17 15:14:24 +0300

    Automated Re-fetching and add baseApi

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -1,43 +1,11 @@
-// https://musicfun.it-incubator.app/api/1.0/
+import type { CreatePlaylistArgs, PlaylistData, PlaylistsResponse, UpdatePlaylistArgs } from './playlistsApi.types'
+import { baseApi } from '@/app/api/baseApi.ts'
 
-
-// Во избежание ошибок импорт должен быть из `@reduxjs/toolkit/query/react`
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import type {
-  CreatePlaylistArgs,
-  FetchPlaylistsArgs,
-  PlaylistData,
-  PlaylistsResponse,
-  UpdatePlaylistArgs
-} from './playlistsApi.types'
-
-// `createApi` - функция из `RTK Query`, позволяющая создать объект `API`
-// для взаимодействия с внешними `API` и управления состоянием приложения
-export const playlistsApi = createApi({
-
-  // `reducerPath` - имя куда будут сохранены состояние и экшены для этого `API`
-  reducerPath: 'playlistsApi',
-
-  // `baseQuery` - конфигурация для `HTTP-клиента`, который будет использоваться для отправки запросов
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    headers: {
-      'API-KEY': import.meta.env.VITE_API_KEY,
-    },
-    prepareHeaders: headers => {
-      headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
-      return headers
-    },
-  }),
-
-  // `endpoints` - метод, возвращающий объект с эндпоинтами для `API`, описанными
-  // с помощью функций, которые будут вызываться при вызове соответствующих методов `API`
-  // (например `get`, `post`, `put`, `patch`, `delete`)
+export const playlistsApi = baseApi.injectEndpoints({
   endpoints: build => ({
-    // Типизация аргументов (<возвращаемый тип, тип query аргументов (`QueryArg`)>)
-    // `query` по умолчанию создает запрос `get` и указание метода необязательно
     fetchPlaylists: build.query<PlaylistsResponse, void>({
       query: () => `playlists`,
+      providesTags: ['Playlist'],
     }),
     createPlaylist: build.mutation<{ data: PlaylistData }, CreatePlaylistArgs>({
       query: body => ({
@@ -45,6 +13,7 @@ export const playlistsApi = createApi({
         method: 'post',
         body,
       }),
+      invalidatesTags: ['Playlist'],
     }),
     deletePlaylist: build.mutation<void, string>({
       query: (playlistId) =>
@@ -52,6 +21,7 @@ export const playlistsApi = createApi({
           url: `playlists/${playlistId}`,
           method: 'delete',
         }),
+      invalidatesTags: ['Playlist'],
     }),
     updatePlaylist: build.mutation<void, { playlistId: string; body: UpdatePlaylistArgs }>({
       query: ({ playlistId, body }) => ({
@@ -59,10 +29,9 @@ export const playlistsApi = createApi({
         method: 'put',
         body,
       }),
+      invalidatesTags: ['Playlist'],
     }),
   }),
 })
 
-// `createApi` создает объект `API`, который содержит все эндпоинты в виде хуков,
-// определенные в свойстве `endpoints`
 export const { useFetchPlaylistsQuery, useCreatePlaylistMutation, useDeletePlaylistMutation, useUpdatePlaylistMutation } = playlistsApi
--- src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
@@ -1,13 +1,20 @@
 import { useDeletePlaylistMutation, useFetchPlaylistsQuery } from '@/features/playlists/api/playlistsApi.ts'
 import s from './PlaylistsPage.module.css'
 import { CreatePlaylistForm } from '@/features/playlists/ui/PlaylistsPage/CreatePlaylistForm/CreatePlaylistForm.tsx'
-import { useUpdatePlaylistMutation } from '../../api/playlistsApi'
+import { useForm } from 'react-hook-form'
+import type { PlaylistData, UpdatePlaylistArgs } from '@/features/playlists/api/playlistsApi.types.ts'
+import { useState } from 'react'
+import { EditPlaylistForm } from '@/features/playlists/ui/PlaylistsPage/EditPlaylistForm.tsx'
+import { PlaylistItem } from '@/features/playlists/ui/PlaylistsPage/PlaylistItem.tsx'
 
 export const PlaylistsPage = () => {
+  const [playlistId, setPlaylistId] = useState<string | null>(null)
+
+  const { register, handleSubmit, reset } = useForm<UpdatePlaylistArgs>()
 
   const { data } = useFetchPlaylistsQuery()
+
   const [deletePlaylist] = useDeletePlaylistMutation()
-  const [updatePlaylist] = useUpdatePlaylistMutation()
 
   const deletePlaylistHandler = (playlistId: string) => {
     if (confirm('Are you sure you want to delete the playlist?')) {
@@ -15,30 +22,44 @@ export const PlaylistsPage = () => {
     }
   }
 
-  const updatePlaylistHandler = (playlistId: string) => {
-    updatePlaylist({
-      playlistId,
-      body: {
-        title: 'update title',
-        description: '2',
-        tagIds: [],
-      },
-    })
+  const editPlaylistHandler = (playlist: PlaylistData | null) => {
+    if (playlist) {
+      setPlaylistId(playlist.id)
+      reset({
+        title: playlist.attributes.title,
+        description: playlist.attributes.description,
+        tagIds: playlist.attributes.tags.map((t) => t.id),
+      })
+    } else {
+      setPlaylistId(null)
+    }
   }
 
   return (
-    <div>
+    <div className={s.container}>
       <h1>Playlists page</h1>
-      <CreatePlaylistForm/>
+      <CreatePlaylistForm />
       <div className={s.items}>
-        {data?.data.map(playlist => {
+        {data?.data.map((playlist) => {
+          const isEditing = playlistId === playlist.id
+
           return (
             <div className={s.item} key={playlist.id}>
-              <div>title: {playlist.attributes.title}</div>
-              <div>description: {playlist.attributes.description}</div>
-              <div>userName: {playlist.attributes.user.name}</div>
-              <button onClick={() => deletePlaylistHandler(playlist.id)}>delete</button>
-              <button onClick={() => updatePlaylistHandler(playlist.id)}>update</button>
+              {isEditing ? (
+                <EditPlaylistForm
+                  playlistId={playlistId}
+                  handleSubmit={handleSubmit}
+                  register={register}
+                  editPlaylist={editPlaylistHandler}
+                  setPlaylistId={setPlaylistId}
+                />
+              ) : (
+                <PlaylistItem
+                  playlist={playlist}
+                  deletePlaylist={deletePlaylistHandler}
+                  editPlaylist={editPlaylistHandler}
+                />
+              )}
             </div>
           )
         })}
